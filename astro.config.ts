92e8bc09054663d7013164c1886b7e539abfbbfb
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2022-08-26 10:29:39 +0300

    more configs

---
+++ astro.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import { astroImageTools } from 'astro-imagetools';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import solid from '@astrojs/solid-js';
+
+/** @type {import('astro').AstroUserConfig} */
+
+import lit from '@astrojs/lit';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    astroImageTools,
+    mdx(),
+    react(),
+    svelte(),
+    solid(),
+    lit(),
+  ],
+});
