5f65f5cd94fe5f8fdbabf447f029007411d8a872
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2022-08-30 00:14:48 +0300

    nav

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import { astroImageTools } from 'astro-imagetools';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
@@ -8,19 +7,20 @@ import solid from '@astrojs/solid-js';
 /** @type {import('astro').AstroUserConfig} */
 
 import lit from '@astrojs/lit';
-
 import partytown from '@astrojs/partytown';
+
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    astroImageTools,
     mdx(),
     react(),
     svelte(),
     solid(),
     lit(),
     partytown({}),
+    image(),
   ],
 });
