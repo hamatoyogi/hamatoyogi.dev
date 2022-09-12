911a6bff836911c2e5d3946de389676454a8143b
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2022-09-12 14:42:31 +0300

    sort SEO

---
+++ src/utils/seo-config.ts
@@ -0,0 +1,16 @@
+export const seoConfig = {
+  baseURL: 'https://hamatoyogi.dev',
+  description:
+    'Yoav Ganbar AKA HamatoYogi · Web Developer, FedBites podcast co-host, community organizer. I love building products, shipping code, talking about it, and writing about it.',
+  type: 'website',
+  image: {
+    url: '/images/hy-graffiti.jpeg',
+    alt: 'Site image', 
+    width: 1200,
+    height: 630,
+  },
+  siteName: 'HamatoYogi.dev', 
+  twitter: {
+    card: 'summary_large_image',
+  },
+};
