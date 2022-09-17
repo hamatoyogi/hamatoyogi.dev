83dcba18adfdb3485ef42dffb1b4dd995851ed74
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2022-09-17 15:41:10 +0300

    update packages

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
@@ -9,24 +8,15 @@ import partytown from '@astrojs/partytown';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
-
 import compress from 'astro-compress';
 
-// https://astro.build/config
 /** @type {import('astro').AstroUserConfig} */
+
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx(),
-    react(),
-    svelte(),
-    solid(),
-    partytown({}),
-    image(),
-    sitemap(),
-    compress(),
-  ],
+  integrations: [tailwind(), mdx(), react(), svelte(), solid(), partytown({}), image(), sitemap(), compress()],
   site: 'https://hamatoyogi.dev',
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
