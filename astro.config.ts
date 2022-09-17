7905840a8cf32242e5dfb16e7a2c4b1493db90c9
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2022-09-17 16:23:08 +0300

    Update pacakges + use Asto-Icon (#5)
    * avatar border
    
    * update packages
    
    * more updates
    
    * svgs
    
    * add svg titles + use sprite in header and footer
    
    * use svg arrows
    
    * use name in footer
    
    * add sitemap custom pages

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
@@ -9,11 +8,11 @@ import partytown from '@astrojs/partytown';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
-
 import compress from 'astro-compress';
 
-// https://astro.build/config
 /** @type {import('astro').AstroUserConfig} */
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
@@ -23,7 +22,16 @@ export default defineConfig({
     solid(),
     partytown({}),
     image(),
-    sitemap(),
+    sitemap({
+      customPages: [
+        'https://hamatoyogi.dev',
+        'https://hamatoyogi.dev/blog',
+        'https://hamatoyogi.dev/blog/why-astro',
+        'https://hamatoyogi.dev/about',
+        'https://hamatoyogi.dev/appearances',
+        'https://hamatoyogi.dev/work',
+      ],
+    }),
     compress(),
   ],
   site: 'https://hamatoyogi.dev',
