407d9d6df1ae32cb359cf0db51fd6af1a90fae7b
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2022-09-17 16:20:28 +0300

    add sitemap custom pages

--- astro.config.ts
+++ astro.config.ts
@@ -12,11 +12,29 @@ import compress from 'astro-compress';
 
 /** @type {import('astro').AstroUserConfig} */
 
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react(), svelte(), solid(), partytown({}), image(), sitemap(), compress()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    react(),
+    svelte(),
+    solid(),
+    partytown({}),
+    image(),
+    sitemap({
+      customPages: [
+        'https://hamatoyogi.dev',
+        'https://hamatoyogi.dev/blog',
+        'https://hamatoyogi.dev/blog/why-astro',
+        'https://hamatoyogi.dev/about',
+        'https://hamatoyogi.dev/appearances',
+        'https://hamatoyogi.dev/work',
+      ],
+    }),
+    compress(),
+  ],
   site: 'https://hamatoyogi.dev',
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
 });
