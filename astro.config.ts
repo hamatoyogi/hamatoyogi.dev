fb1c59822551b9b9551b4757ddada47e130203a5
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2022-09-17 10:17:22 +0300

    add compress + fix nav responsiveness fix dev script

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 import solid from '@astrojs/solid-js';
-/** @type {import('astro').AstroUserConfig} */
-
 import partytown from '@astrojs/partytown';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
-
 import vercel from '@astrojs/vercel/serverless';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
+/** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   integrations: [
     tailwind(),
@@ -23,6 +24,7 @@ export default defineConfig({
     partytown({}),
     image(),
     sitemap(),
+    compress(),
   ],
   site: 'https://hamatoyogi.dev',
   output: 'server',
