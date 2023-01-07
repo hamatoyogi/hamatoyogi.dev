c5a5b8c207c29140da407cf2927b07c298ca987b
Yoav Ganbar <yoav@builder.io>
2023-01-07 20:16:15 +0200

    use astro components instead of svgs and astro/icon

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as GithubLogo } from './GithubLogo.astro';
+export { default as TwitterLogo } from './TwitterLogo.astro';
+export { default as External } from './External.astro';
+export { default as ArrowRight } from './ArrowRight.astro';
