6a38c6cd096d4ab81bcd7cfc57b73b2aeaac9171
Yoav Ganbar <yoav@builder.io>
2023-11-10 11:50:25 +0200

    Update dependencies and imports

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
-import svelte from '@astrojs/svelte';
-import solid from '@astrojs/solid-js';
 import partytown from '@astrojs/partytown';
-import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
 import compress from 'astro-compress';
@@ -19,11 +15,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     mdx(),
-    react(),
-    svelte(),
-    solid(),
     partytown({}),
-    image(),
     sitemap({
       // TODO find a way to load dynamically
       customPages: [
