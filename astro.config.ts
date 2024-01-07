9f58e7fb73e96ef4b3222916542b4eea3037861a
Yoav Ganbar <yoav@builder.io>
2024-01-07 18:16:30 +0200

    remove deprecated @astro/prefetch. update all packages

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
 import compress from 'astro-compress';
-import prefetch from '@astrojs/prefetch';
 import qwikdev from '@qwikdev/astro';
 const SITE_URL = 'https://hamatoyogi.dev';
 /** @type {import('astro').AstroUserConfig} */
@@ -33,10 +32,10 @@ export default defineConfig({
       ],
     }),
     compress(),
-    prefetch(),
     qwikdev(),
   ],
   site: SITE_URL,
   output: 'server',
   adapter: vercel(),
+  prefetch: true,
 });
