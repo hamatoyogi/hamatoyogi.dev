0967b6953bfc44380a9e936a60aed86748b8f1ea
Yoav Ganbar <hamatoyogi@users.noreply.github.com>
2025-08-02 12:04:58 +0300

    Remove unused @astrojs/partytown integration and dependencies from configuration files (#25)
    * Remove unused @astrojs/partytown integration and dependencies from configuration files
    
    * update astro + Remove view transitions

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 import compress from 'astro-compress';
@@ -15,7 +14,6 @@ export default defineConfig({
   integrations: [
     qwikdev(),
     mdx(),
-    partytown({}),
     sitemap({
       // TODO find a way to load dynamically
       customPages: [
